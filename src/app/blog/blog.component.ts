91bcd2dabeb66b817dc6200fe98e08e04b48ab46
angular654 <v.ivahnenko@outlook.com>
2021-05-16 10:25:16 +0300

    blog page design

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogService } from '../blog.service';
+interface Post {
+  _id:string
+  heading: string;
+  description: string;
+  article: string;
+  topic: string;
+  author: string;
+}
 
 @Component({
   selector: 'app-blog',
@@ -6,10 +15,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent implements OnInit {
+  data = []
+  topics = [{
+    title: 'Eco energy',
+    img: "../../assets/energy-cat.svg"
+  },{
+    title: 'Ecology',
+    img: "../../assets/eco-icon.svg"
+  },
+  {
+    title: 'Recycling',
+    img: "../../assets/recycling-cat.svg"
+  },
+  {
+    title: 'Alternative energy',
+    img: "../../assets/alt-energy-cat.svg"
+  },
+  {
+    title: 'Eco fuel',
+    img: "../../assets/eco-fuel-cat.svg"
+  },
+  {
+    title: 'Solar energy',
+    img: "../../assets/solar-energy-cat.svg"
+  }]
+  cat = null
+  constructor(private blog:BlogService) { }
+  range = 3
 
-  constructor() { }
-
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    await this.getPostsByCat()
   }
+  async deletePost() {
 
+  }
+  async getPostsByCat() {
+    await this.blog.findByTopic(this.cat).then((res:Post[])=>{
+      this.data = res
+      console.log(this.data)
+    })
+  }
+  readMore(){
+    this.range = this.data.length
+  }
 }
