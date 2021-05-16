8686bbc4e9dd99f750d15dd34d162c55248d80cd
angular654 <v.ivahnenko@outlook.com>
2021-05-16 09:40:22 +0300

    creating posts done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from './auth.guard';
 import { AuthComponent } from './auth/auth.component';
 import { BlogComponent } from './blog/blog.component';
 import { CreateEcoEventComponent } from './create-eco-event/create-eco-event.component';
+import { CreatePostComponent } from './create-post/create-post.component';
 import { EcoEventInfoComponent } from './eco-event-info/eco-event-info.component';
 import { GeoFindComponent } from './geo-find/geo-find.component';
 import { MainPageComponent } from './main-page/main-page.component'
@@ -15,6 +16,7 @@ const routes: Routes = [
   {path: '', component: MainPageComponent},
   {path: 'auth', component: AuthComponent},
   {path: 'create-eco-event', component: CreateEcoEventComponent, canActivate: [AuthGuard]},
+  {path: 'create-post', component: CreatePostComponent, canActivate: [AuthGuard]},
   {path: 'eco/:id', component: EcoEventInfoComponent,canActivate: [AuthGuard]},
   {path: 'article/:id', component: PostComponent, canActivate: [AuthGuard]},
   {path: 'parks', component: ParksComponent, canActivate: [AuthGuard]},
